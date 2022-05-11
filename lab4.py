045c9b457cce97853e2d07d916f8fd9a6f8305cc
Lan Nguyen <74268571+nguyentlan@users.noreply.github.com>
2022-05-10 20:07:58 -0700

    added menu

--- lab4.py
+++ lab4.py
@@ -53,72 +53,211 @@ try:
     connection.commit()
     #connection.close()
 
-    # Question 1
-    # StartLocationName = input('Please enter a Start Location Name: ')
-    # DestinationName = input('Please enter a Destination Name: ')
-    # Date = input('Please enter a Date:')
-    # for row in cursor.execute('''   SELECT 
-    #                                 t.StartLocationName, t.DestinationName, f.Date, f.ScheduledStartTime, f.ScheduledArrivalTime, f.DriverName, f.BusID 
-    #                                 FROM 
-    #                                 Trip t, TripOffering f 
-    #                                 WHERE 
-    #                                 t.TripNumber = f.TripNumber AND t.StartLocationName = "%s" AND t.DestinationName = "%s" AND f.Date = "%s"
-    #                           ''' % (StartLocationName, DestinationName, Date)):
-    #     print(row)
-        
-    # # Question 2
-    
-    # # Question 3 
-    # Trip = input('Enter a trip: ')
-    # for row in cursor.execute('''   SELECT
-    #                                 t.StopNumber
-    #                                 FROM
-    #                                 TripStopInfo t
-    #                                 WHERE
-    #                                 t.TripNumber = "%s"
-    #                           ''' % (Trip)):
-    #     print(row)
-    
-    # Done
-    # Question 4
-    # driverName = input("Please enter driver name:")
-    # date = input("Please enter the date the driver is supposed to drive:")
-    # for row in cursor.execute('''SELECT * FROM TripOffering WHERE DriverName = "%s" AND Date = "%s" ''' % (driverName, date)):
-    #     print(row)
+    cursor.execute
 
-    # Done 
-    # # Question 5
-    # driverName = input("Please enter the driver name you would like to add:")
-    # driverTelephone = input("Enter the phone number:")
-    # for row in cursor.execute('''INSERT INTO Driver(DriverName, DriverTelephoneNumber) VALUES("%s", "%s")''' % (driverName, driverTelephone)):
-    #     print(row)
-    
-    # Question 6
-    # busID = input("Please enter the new BusID:")
-    # busPlate = input("Please enter the bus plate:")
-    # busYear = input("Please enter the year:")
-    # busMake = input("Please enter the make:")
-    # cursor.execute('''INSERT INTO Bus(BusID, Plate, Year, Make) VALUES("%s", "%s", "%s", "%s")''' % (busID, busPlate, busYear, busMake))
+    menu = False
 
-    # for row in cursor.execute('''SELECT * FROM Bus'''):
-    #     print(row)
+    while menu is True:
 
-    # Question 7
-    BusID = input('Enter a BusID to delete: ')
-    print('Here is your old table: ')
-    for row in cursor.execute(''' SELECT *
-                                  FROM Bus
-    '''):
-        print(row)
-    cursor.execute(''' DELETE FROM
-                                  Bus
-                                  WHERE
-                                  BusID = "%s"
-    ''' % (BusID))
-    print('BusID has been deleted, here is your new Bus Table: ')
-    for row in cursor.execute(''' SELECT *
-                                  FROM Bus
-    '''):
-        print(row)
+        menuChoice = input("""Please choose an option from the menu
+                <1>  Question 1 
+                <2>  Question 2 
+                <3>  Question 3 
+                <4>  Question 4 
+                <5>  Question 5 
+                <6>  Question 6 
+                <7>  Question 7 
+                <8>  Question 8 
+                <9>  Quit \n""")
+
+        if menuChoice == '1':
+            print("Display the schedule of all trips given dt")
+            StartLocationName = input('Please enter a Start Location Name: ')
+            DestinationName = input('Please enter a Destination Name: ')
+            Date = input('Please enter a Date:')
+            for row in cursor.execute('''   SELECT 
+                                            t.StartLocationName, t.DestinationName, f.Date, f.ScheduledStartTime, f.ScheduledArrivalTime, f.DriverName, f.BusID 
+                                            FROM 
+                                            Trip t, TripOffering f 
+                                            WHERE 
+                                            t.TripNumber = f.TripNumber AND t.StartLocationName = "%s" AND t.DestinationName = "%s" AND f.Date = "%s"
+                                      ''' % (StartLocationName, DestinationName, Date)):
+                print(row)
+
+        if menuChoice == '2':
+            question2 = True
+
+            while question2 is True:
+                question2Choice = input("""Please choose an option from the menu
+                <1>  Delete a Trip Offering
+                <2>  Add a Trip Offering
+                <3>  Change the Driver of a Trip Offering
+                <4>  Change the Bus of a Trip Offering
+                <5>  Quit\n""")
+
+                if question2Choice == '1':
+                    tripNumber = input('Please specify the Trip Number: ')
+                    dateNumber = input("Please input the Trip's Date: ")
+                    scheduledStartTime = input("Please input the Trip's Starting Time: ")
+
+                    print("Here is the old version of the table: ")
+                    for row in cursor.execute(''' SELECT * FROM TripOffering'''):
+                        print(row)
+
+                    cursor.execute(''' DELETE FROM TripOffering WHERE
+                                TripNumber = "%s" AND Date = "%s" AND ScheduledStartTime = "%s" ''' 
+                                % (tripNumber, dateNumber, scheduledStartTime))
+
+                    print("Here is the updated version of the table: ")
+                    for row in cursor.execute(''' SELECT * FROM TripOffering'''):
+                        print(row)
+
+                    userChoice = input("Would you like to continue with Question 2? (Y/N)")
+                    if userChoice == "Y":
+                        question2 = True
+                    if userChoice == "N":
+                        question2 = False
+
+                if question2Choice == '2':
+                    tripNumber = input("Please specify the Trip Number: ")
+                    dateNumber = input("Please input the Trip's Date: ")
+                    scheduledStartTime = input("Please input the Trip's Starting Time: ")
+                    scheduledArrivalTime = input("Please input the Trip's Arrival Time: ")
+                    driverName = input("Please input the Trip's Driver Name: ")
+                    busID = input("Please input the Trip's busID: ")
+                    
+                    print("Here is the old version of the table: ")
+                    for row in cursor.execute(''' SELECT * FROM TripOffering'''):
+                        print(row)
+                    
+                    cursor.execute('''INSERT INTO TripOffering(TripNumber, Date, ScheduledStartTime, ScheduledArrivalTime,
+                                DriverName, BusID) VALUES("%s","%s","%s","%s","%s", "%s")'''
+                                % (tripNumber, dateNumber, scheduledStartTime, scheduledArrivalTime, driverName, busID))
+                    
+                    print("Here is the updated version of the table: ")
+                    for row in cursor.execute(''' SELECT * FROM TripOffering'''):
+                        print(row)
+                    
+                    userChoice = input("Would you like to continue with Question 2? (Y/N)")
+                    if userChoice == "Y":
+                        question2 = True
+                    if userChoice == "N":
+                        question2 = False
+
+                if question2Choice == '3':
+                    tripNumber = input('Please specify the Trip Number: ')
+                    dateNumber = input("Please input the Trip's Date: ")
+                    scheduledStartTime = input("Please input the Trip's Starting Time: ")
+                    driverName = input("Please input the Driver Change for this Trip: ")
+
+                    print("Here is the old version of the table: ")
+                    for row in cursor.execute(''' SELECT * FROM TripOffering'''):
+                        print(row)
+
+                    cursor.execute('''UPDATE TripOffering SET DriverName = "%s" WHERE TripNumber = "%s" AND
+                                Date = "%s" AND ScheduledStartTime = "%s"''' % (driverName, tripNumber, dateNumber, 
+                                scheduledStartTime))
+
+                    print("Here is the updated version of the table: ")
+                    for row in cursor.execute(''' SELECT * FROM TripOffering'''):
+                        print(row)
+
+                    userChoice = input("Would you like to continue with Question 2? (Y/N)")
+                    if userChoice == "Y":
+                        question2 = True
+                    if userChoice == "N":
+                        question2 = False
+                    
+                if question2Choice == '4':
+                    tripNumber = input('Please specify the Trip Number: ')
+                    dateNumber = input("Please input the Trip's Date: ")
+                    scheduledStartTime = input("Please input the Trip's Starting Time: ")
+                    busID = input("Please input the Bus ID Change for this Trip: ")
+
+                    print("Here is the old version of the table: ")
+                    for row in cursor.execute(''' SELECT * FROM TripOffering'''):
+                        print(row)
+
+                    cursor.execute('''UPDATE TripOffering SET busID = "%s" WHERE TripNumber = "%s" AND
+                                Date = "%s" AND ScheduledStartTime = "%s"''' % (busID, tripNumber, dateNumber, 
+                                scheduledStartTime))
+
+                    print("Here is the updated version of the table: ")
+                    for row in cursor.execute(''' SELECT * FROM TripOffering'''):
+                        print(row)
+
+                if question2Choice == '5':
+                    question2 = False
+        if menuChoice == '3':
+            print("Display the stops of a given trip")
+            Trip = input('Enter a trip: ')
+            for row in cursor.execute('''   SELECT
+                                            t.StopNumber
+                                            FROM
+                                            TripStopInfo t
+                                            WHERE
+                                            t.TripNumber = "%s"
+                                        ''' % (Trip)):
+                print(row)
+
+
+        if menuChoice == '4':
+            print("Display the weekly schedule of a given driver and date")
+            driverName = input("Please enter driver name:")
+            date = input("Please enter the date the driver is supposed to drive:")
+            for row in cursor.execute('''SELECT * FROM TripOffering WHERE DriverName = "%s" AND Date = "%s" ''' % (driverName, date)):
+                print(row)
+
+        if menuChoice == '5':
+            print("Add a driver")
+            driverName = input("Please enter the driver name you would like to add:")
+            driverTelephone = input("Enter the phone number:")
+            print("Here is the old version of the table")
+            for row in cursor.execute(''' SELECT * FROM Driver'''):
+                print(row)
+            for row in cursor.execute('''INSERT INTO Driver(DriverName, DriverTelephoneNumber) VALUES("%s", "%s")''' % (driverName, driverTelephone)):
+                print(row)
+
+            print("Here is the updated version of the table")
+            for row in cursor.execute(''' SELECT * FROM Driver'''):
+                print(row)
+
+        if menuChoice == '6':
+            print("Add a Bus")
+            busID = input("Please enter the new BusID:")
+            busPlate = input("Please enter the bus plate:")
+            busYear = input("Please enter the year:")
+            busMake = input("Please enter the make:")
+            print("Here is the old version of the table")
+            for row in cursor.execute(''' SELECT * FROM Bus'''):
+                print(row)
+            cursor.execute('''INSERT INTO Bus(BusID, Plate, Year, Make) VALUES("%s", "%s", "%s", "%s")''' % (busID, busPlate, busYear, busMake))
+            print("Here is the updated table")
+            for row in cursor.execute('''SELECT * FROM Bus'''):
+                print(row)
+
+        if menuChoice == '7':
+            print("Delete a BusID")
+            BusID = input('Enter a BusID: ')
+            print('Here is your old table: ')
+            for row in cursor.execute(''' SELECT *
+                                        FROM Bus
+            '''):
+                print(row)
+            cursor.execute(''' DELETE FROM
+                                        Bus
+                                        WHERE
+                                        BusID = "%s"
+            ''' % (BusID))
+            print('BusID has been deleted, here is your new Bus Table: ')
+            for row in cursor.execute(''' SELECT *
+                                        FROM Bus
+            '''):
+                print(row)
+
+        # if menuChoice == '8':
+
+        if menuChoice == '9':
+            menu = False
 except Error as e: 
     print(e)
